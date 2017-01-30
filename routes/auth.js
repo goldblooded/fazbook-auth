87c60e96cb56d907ba760bae187daf8cbf81678f
Jason John <jkj1982@gmail.com>
2017-01-30 05:02:15 -0500

    add auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -7,3 +7,29 @@ const passport = require('..auth/local');
 router.get('/register', authHelpers.loginRedirect, (req, res)=> {
   res.render('auth/register');
 });
+
+router.post('/register', (req, res, next) => {
+  return authHelpers.createUser(req, res)
+  .then((response) => {
+    console.log('registration successful');
+  })
+  .catch((err) => { res.statis(500).json({ status: 'error' }); });
+});
+
+router.get('/login', authHelpers.loginRedirect, (req,res) => {
+  res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+  })
+);
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
