a93dab7d0c3529266f57907bb7e6730fec793543
Jason John <jkj1982@gmail.com>
2017-01-30 04:23:52 -0500

    updated

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -2,8 +2,16 @@ const bcrypt = require('bcryptjs');
 
 const models = require('../db/models/index');
 
-// function to compare passwords (entered v in database).
+// function to compare passwords (user entry vs in database).
 // bcrypt is password hashing function
 function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
+}
+
+function loginRedirect(req, res, next) {
+  if (req.user) return res.status(401).json(
+    { status: 'You are already logged in' }
+  );
+
+  return next();
 }
